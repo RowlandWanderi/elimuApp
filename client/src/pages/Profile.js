45e9d57bdbe76672c084c5f2a76ac7af774d3274
rowlandwanderi <rowlandswanderi@gmail.com>
2024-10-17 14:48:33 +0000

    updated profile page

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -8,15 +8,14 @@ const {currentUser,updateUser, deleteAccount} = useContext(UserContext)
 
 const [username, setUsername] = useState(currentUser && currentUser.username)
 const [email, setEmail] = useState(currentUser && currentUser.email)
-const [phone_number, setPhone] = useState(currentUser && currentUser.phone_number)
-const [profile_image_url, setProfileImageUrl] = useState(currentUser && currentUser.profile_image_url)
+
 
 
 	const handleSubmit = (e)=>{
 	e.preventDefault()
 
 	// call your useContext function
-	updateUser(username,email, phone_number,profile_image_url)
+	updateUser(username,email)
 	
 	
 	}
@@ -27,14 +26,7 @@ const [profile_image_url, setProfileImageUrl] = useState(currentUser && currentU
           <div className="col col-lg-6 mb-4 mb-lg-0">
             <div className="card mb-3" style={{ borderRadius: '.5rem' }}>
               <div className="row g-0">
-			  <div className="col-md-4 gradient-custom text-center text-white" style={{ borderTopLeftRadius: '.5rem', borderBottomLeftRadius: '.5rem' }}>
-				<img
-					src={currentUser && currentUser.profile_image_url || 'https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-chat/ava1-bg.webp'}
-					alt="Avatar"
-					className="img-fluid my-4"
-					style={{ width: '90px' }}
-				/>
-				</div>
+			 
                 <div className="col-md-8">
                   <div className="card-body p-4">
                     <h6>Personal Details</h6>
@@ -47,10 +39,6 @@ const [profile_image_url, setProfileImageUrl] = useState(currentUser && currentU
                       <div className="col-6 mb-3">
                         <h6>Email</h6>
                         <p className="text-muted">{currentUser && currentUser.email}</p>
-                      </div>
-                      <div className="col-6 mb-3">
-                        <h6>Phone</h6>
-                        <p className="text-muted">{currentUser && currentUser.phone_number}</p>
                       </div>
                     </div>
                   </div>
@@ -73,18 +61,6 @@ const [profile_image_url, setProfileImageUrl] = useState(currentUser && currentU
             <label className="form-label col-sm-2">Email address</label>
             <div className="col-sm-10">
               <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="form-control" />
-            </div>
-          </div>
-          <div className="form-group row my-4">
-            <label className="form-label col-sm-2">Phone</label>
-            <div className="col-sm-10">
-              <input type="text" value={phone_number} onChange={(e) => setPhone(e.target.value)} className="form-control" />
-            </div>
-          </div>
-          <div className="form-group row my-4">
-            <label className="form-label col-sm-2">Profile image url</label>
-            <div className="col-sm-10">
-              <input type="text" value={profile_image_url} onChange={(e) => setProfileImageUrl(e.target.value)} className="form-control" />
             </div>
           </div>
           <button type="submit" className="btn btn-success w-40">
