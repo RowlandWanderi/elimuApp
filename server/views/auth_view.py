d67f97fbff649a1c894efe66991494d17bf1ec2a
rowlandwanderi <rowlandswanderi@gmail.com>
2024-10-17 08:47:07 +0000

    add authentication views

--- server/views/auth_view.py
+++ server/views/auth_view.py
@@ -8,3 +8,81 @@ from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identi
 auth_bp = Blueprint('auth_bp', __name__)
 
 
+# routes
+# login user
+@auth_bp.route("/login", methods=["POST"])
+def login():
+    data = request.get_json()
+    username = data.get('username')  
+    password = data.get('password')
+    
+    user = User.query.filter_by(username = username).first()
+
+    if user:
+        
+        if check_password_hash(user.password, password):
+            access_token = create_access_token(identity=user.id)
+            return jsonify(access_token = access_token)
+        
+        print("Debug: Incorrect Password")
+        return jsonify({"error": "Wrong Password!"}), 401
+
+    else:
+        print("Debug: User Not Found")
+        return jsonify({"error": "User doesn't exist!"}), 404
+    
+# Get logged in user
+@auth_bp.route("/authenticated_user", methods=["GET"])
+@jwt_required()
+def authenticated_user():
+    current_user_id = get_jwt_identity()  # getting current user id
+    user = User.query.get(current_user_id)
+
+    if user:
+        user_data = {
+            'id': user.id,
+            'username': user.username,
+            'email': user.email,
+            
+            
+        }
+        return jsonify(user_data), 200
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+# Logout user
+@auth_bp.route("/logout", methods=["POST"])
+@jwt_required()
+def logout():
+    jwt = get_jwt()
+
+    jti = jwt['jti']
+
+    token_b = TokenBlocklist(jti=jti)
+    db.session.add(token_b)
+    db.session.commit()
+
+    return jsonify({"success": "Logged out successfully!"}), 200
+
+
+# Reset user password
+@auth_bp.route("/reset_password", methods=["POST"])
+def reset_password():
+    data = request.get_json()
+    email = data.get("email")
+    new_password = data.get("new_password")
+
+    user = User.query.filter_by(email=email).first()
+
+    if user:
+        # Hash the new password before storing it
+        hashed_password = generate_password_hash(new_password)
+
+        # Update the user's password
+        user.password = hashed_password
+        db.session.commit()
+
+        return jsonify({"success": "Password reset successful"}), 200
+    else:
+        return jsonify({"error": "User with the provided email not found"}), 404
